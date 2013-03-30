adc27a303c26a5e0620ee31f8b9c146a982441ec
solzenic <solzenic@sola.local>
2013-03-30 16:53:43 +0100

    Fix in namespace.

--- Form/Type/DateTimeRangeType.php
+++ Form/Type/DateTimeRangeType.php
@@ -1,19 +1,9 @@
 <?php
-/*
- * This file is part of the Sonata package.
- *
- * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- */
 
-namespace Sonata\AdminBundle\Form\Type;
+namespace Imatic\Bundle\FormBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-
 use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
