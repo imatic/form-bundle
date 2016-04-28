24206e61930a3f11d1bb89860c91b064026cafbd
Pavel Batecko <pavel.batecko@imatic.cz>
2016-04-28 16:41:56 +0200

    RESEARCH-168 - Zruseni zavislosti na GenemuBundle

---
+++ Tests/Unit/Form/Type/DateRangeTypeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Imatic\Bundle\FormBundle\Tests\Unit\Form\Type;
+
+use Imatic\Bundle\FormBundle\Form\Type\DateRangeType;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+/**
+ * @author Miloslav Nenadal <miloslav.nenadal@imatic.cz>
+ */
+class DateRangeTypeTest extends TypeTestCase
+{
+    public function testSubmitValidData()
+    {
+        $formData = [
+            'start' => [
+                'year' => 2014,
+                'month' => 3,
+                'day' => 5,
+            ],
+            'end' => [
+                'year' => 2014,
+                'month' => 05,
+                'day' => 10,
+            ],
+        ];
+
+        $form = $this->factory->create(DateRangeType::class);
+        $form->submit($formData);
+
+        $this->assertTrue($form->has('start'));
+        $this->assertTrue($form->has('end'));
+        $this->assertEquals('2014-03-05 00:00:00', $form->get('start')->getData()->format('Y-m-d H:i:s'));
+        $this->assertEquals('2014-05-10 00:00:00', $form->get('end')->getData()->format('Y-m-d H:i:s'));
+    }
+}
---
+++ Tests/Unit/Form/Type/TimeRangeTypeTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Imatic\Bundle\FormBundle\Tests\Unit\Form\Type;
+
+use Imatic\Bundle\FormBundle\Form\Type\TimeRangeType;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+/**
+ * @author Miloslav Nenadal <miloslav.nenadal@imatic.cz>
+ */
+class TimeRangeTypeTest extends TypeTestCase
+{
+    public function testSubmitValidData()
+    {
+        $formData = [
+            'start' => [
+                'hour' => 13,
+                'minute' => 15,
+            ],
+            'end' => [
+                'hour' => 5,
+                'minute' => 59,
+            ],
+        ];
+
+        $form = $this->factory->create(TimeRangeType::class);
+        $form->submit($formData);
+
+        $this->assertTrue($form->has('start'));
+        $this->assertTrue($form->has('end'));
+        $this->assertEquals('1970-01-01 13:15:00', $form->get('start')->getData()->format('Y-m-d H:i:s'));
+        $this->assertEquals('1970-01-01 05:59:00', $form->get('end')->getData()->format('Y-m-d H:i:s'));
+    }
+}
