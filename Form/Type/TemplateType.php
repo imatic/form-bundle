9134337f48755f241f984bd656b9780740ac854b
Štěpán Kočí <stepan.koci@imatic.cz>
2017-03-02 00:18:25 +0100

    Template type added.

---
+++ Form/Type/TemplateType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Imatic\Bundle\FormBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TemplateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired([
+            'template',
+        ]);
+        $resolver->setDefaults([
+            'mapped' => false,
+            'required' => false,
+            'label' => false,
+        ]);
+    }
+}
