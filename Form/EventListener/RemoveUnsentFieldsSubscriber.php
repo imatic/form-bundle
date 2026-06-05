da5258138d4a9cb2342be13ead3c6f89ba82cd7a
Jaroslav S <mail@example.com>
2026-06-05 13:40:37 +0200

    symfony-8.1 fixed deprecations

--- Form/EventListener/RemoveUnsentFieldsSubscriber.php
+++ Form/EventListener/RemoveUnsentFieldsSubscriber.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\FormEvents;
  */
 class RemoveUnsentFieldsSubscriber implements EventSubscriberInterface
 {
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             FormEvents::PRE_SUBMIT => 'preSubmit',
